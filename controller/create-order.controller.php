80f1034b288fbc403f392db1ed66389858d8ef11
David Robert <contact@david-robert.fr>
2025-03-25 14:49:41 +0100

    order form quantity

---
+++ controller/create-order.controller.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once('../config.php');
+
+
+require_once('../view/create-order.view.php');
---
+++ view/create-order.view.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+
+	<header>
+
+		<nav>
+			<ul>
+				<li>Créer une commande</li>
+			</ul>
+		</nav>
+
+	</header>
+
+	<main>
+
+		<form method="GET">
+
+			<label for="quantity">Quantity
+				<input type="number" name="quantity" />
+			</label>
+
+			<button type="submit">Créer la commande</button>
+
+		</form>
+
+	</main>
+
+</body>
+</html>
