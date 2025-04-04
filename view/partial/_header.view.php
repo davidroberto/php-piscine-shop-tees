ce903ae50b7ffd000ad9971c6b40bdf9357d8873
David Robert <contact@david-robert.fr>
2025-04-04 14:37:06 +0200

    header

---
+++ view/partial/_header.view.php
@@ -0,0 +1,22 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+
+	<header>
+
+		<nav>
+			<ul>
+				<li><a href="http://localhost:8888/piscine-shop-tees/controller/create-order.controller.php">Créer une commande</a></li>
+				<li><a href="http://localhost:8888/piscine-shop-tees/controller/pay-order.controller.php">Payer la commande</a></li>
+			</ul>
+		</nav>
+
+	</header>
+
--- view/pay-order.view.php
+++ view/pay-order.view.php
@@ -1,24 +1,4 @@
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
-</head>
-<body>
-
-	<header>
-
-		<nav>
-			<ul>
-				<li>Créer une commande</li>
-			</ul>
-		</nav>
-
-	</header>
-
+<?php require('../view/partial/_header.view.php'); ?>
 
 	<main>
 	
